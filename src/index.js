7f52f2f24774c96787d5f6781669779b2d1038c1
Jhovahn <jhovahn@Jhovahns-MacBook-Air.local>
2018-06-22 12:06:26 -0700

    added middleware, completed redux implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+
 import './index.css';
 import 'tachyons';
-import App from './App';
-import Card from './Card';
-import CardList from './CardList';
-import { robots } from './robots.js';
+
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { searchRobots, requestRobots } from './reducers';
+
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
+
+console.log(store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
