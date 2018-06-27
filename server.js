44794ab3f8bfb70886a7f8c41f202d864b2fe01f
Jhovahn <jhovahn@Jhovahns-MacBook-Air.local>
2018-06-27 16:56:05 -0700

    server changes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'src')))
-  .get('/', (req, res) => res.render('build'))
+  .use(express.static(path.join(__dirname, 'build')))
+  .get('*', (req, res) =>
+    res.sendFile(path.join(__dirname + 'build/index.html'))
+  )
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
